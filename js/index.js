dfa3ec5c242c939203468ce9ffbc66fd7fe13733
Otabek <abdullayevriza80@gmail.com>
2026-01-30 00:01:50 +0500

    wert

---
+++ js/index.js
@@ -0,0 +1,284 @@
+const body=document.getElementsByTagName("body")
+console.log(body)
+body[0].style.padding="0"
+body[0].style.margin="auto"
+body[0].style.maxWidth="1920px"
+body[0].style.width=" 1920px"
+
+
+const header=document.getElementById("header")
+console.log(header)
+header.style.display="flex"
+header.style.alignItems="center"
+header.style.width="100%"
+header.style.height="150px"
+header.style.justifyContent="space-between"
+header.style.background="#000"
+header.style.padding="0 300px"
+
+const imgs=document.getElementById("imgs")
+console.log(imgs)
+imgs.style.width="120px"
+
+
+
+// header__nav
+const header__nav=document.getElementsByClassName("header__nav")
+console.log(header__nav)
+header__nav[0].style.display="flex"
+header__nav[0].style.alignItems="center"
+header__nav[0].style.gap="30px"
+
+
+// header__nav--link
+const header__navlink=document.querySelectorAll(".header__nav--link")
+console.log(header__navlink)
+header__navlink[0].style.color="#fff"
+header__navlink[1].style.color="#fff"
+header__navlink[2].style.color="#fff"
+header__navlink[3].style.color="#fff"
+header__navlink[4].style.color="#fff"
+
+
+
+
+// section
+const section=document.getElementsByClassName("section")
+console.log(section)
+section[0].style.width="100%"
+section[0].style.height="700px"
+section[0].style.padding=" 0 300px"
+section[0].style.background="#000"
+section[0].style.display="flex"
+section[0].style.alignItems="center"
+section[0].style.justifyContent="space-between"
+section[0].style.margin="0 0 100px 0"
+
+const section__text=document.getElementsByClassName("section__text")
+console.log(section__text)
+section__text[0].style.width="545px"
+section__text[0].style.margin="0 0 80px 0"
+section__text[0].style.fontSize="60px"
+section__text[0].style.fontWeight="600"
+section__text[0].style.color="#fff"
+
+
+// hero
+const hero=document.getElementsByClassName("hero")
+console.log(hero)
+hero[0].style.display="flex"
+hero[0].style.alignItems="center"
+hero[0].style.justifyContent="space-between"
+hero[0].style.margin="0 0 100px 0"
+hero[0].style.padding=" 0 300px"
+
+const hero__text=document.getElementsByClassName("hero__text")
+console.log(hero__text)
+
+hero__text[0].style.width="472px"
+hero__text[0].style.fontSize="42px"
+hero__text[0].style.fontWeight="700"
+hero__text[0].style.margin="0 0 30px 0"
+
+const hero__paragrf=document.getElementsByClassName("hero__paragrf")
+console.log(hero__paragrf)
+hero__paragrf[0].style.width="470px"
+hero__paragrf[0].style.fontSize="16px"
+hero__paragrf[0].style.fontWeight="400"
+hero__paragrf[0].style.color="#434343"
+hero__paragrf[0].style.margin="0 0 60px 0"
+
+
+const hero__btn=document.getElementsByClassName("hero__btn")
+console.log(hero__btn)
+hero__btn[0].style.fontSize="14px"
+hero__btn[0].style.fontWeight="500"
+hero__btn[0].style.padding="12px 30px"
+hero__btn[0].style.color="#fff"
+hero__btn[0].style.background="#000"
+hero__btn[0].style.border="none"
+
+const imgss=document.getElementById("imgss")
+console.log(imgss)
+imgss.style.width="571px"
+imgss.style.height="552px"
+
+
+// hero2
+const hero2=document.getElementsByClassName("hero2")
+console.log(hero2)
+hero2[0].style.display="flex"
+hero2[0].style.alignItems="center"
+hero2[0].style.justifyContent="space-between"
+hero2[0].style.margin="0 0 100px 0"
+hero2[0].style.padding=" 0 300px"
+
+const hero2__img=document.getElementById("hero2__img")
+console.log(hero2__img)
+hero2__img.style.width="571px"
+hero2__img.style.height="704px"
+
+const hero2__text=document.getElementsByClassName("hero2__text")
+console.log(hero2__text)
+
+hero2__text[0].style.display="flex"
+hero2__text[0].style.width="451px"
+hero2__text[0].style.justifyContent="center"
+hero2__text[0].style.fontWeight="700"
+hero2__text[0].style.fontSize="40px"
+hero2__text[0].style.margin="auto"
+hero2__text[0].style.margin="0 0 50px 0"
+
+
+const quti=document.getElementsByClassName("quti")
+console.log(quti)
+
+quti[0].style.display="flex"
+quti[0].style.alignItems="center"
+quti[0].style.gap="30px"
+quti[0].style.margin="0 0 55px 0"
+
+
+quti[1].style.display="flex"
+quti[1].style.alignItems="center"
+quti[1].style.gap="30px"
+quti[1].style.margin="0 0 55px 0"
+
+
+quti[2].style.display="flex"
+quti[2].style.alignItems="center"
+quti[2].style.gap="30px"
+
+
+const teg =document.getElementsByTagName("h3")
+console.log(teg)
+
+teg[1].style.fontSize="22px"
+teg[1].style.fontWeight="700"
+teg[1].style.color="#000"
+teg[1].style.margin="0 0 30px 0"
+
+
+teg[2].style.fontSize="22px"
+teg[2].style.fontWeight="700"
+teg[2].style.color="#000"
+teg[2].style.margin="0 0 30px 0"
+
+teg[3].style.fontSize="22px"
+teg[3].style.fontWeight="700"
+teg[3].style.color="#000"
+teg[3].style.margin="0 0 30px 0"
+
+
+const teg2 =document.getElementsByTagName("p")
+console.log(teg2)
+
+teg2[1].style.fontSize="14px"
+teg2[1].style.fontWeight="400"
+teg2[1].style.color="#434343"
+teg2[1].style.width="389px"
+
+
+teg2[2].style.fontSize="14px"
+teg2[2].style.fontWeight="400"
+teg2[2].style.color="#434343"
+teg2[2].style.width="389px"
+
+teg2[3].style.fontSize="14px"
+teg2[3].style.fontWeight="400"
+teg2[3].style.color="#434343"
+teg2[3].style.width="389px"
+
+
+
+// hero3
+const hero3=document.getElementsByClassName("hero3")
+console.log(hero3)
+hero3[0].style.width="100%"
+hero3[0].style.height="410px"
+hero3[0].style.display="flex"
+hero3[0].style.alignItems="center"
+hero3[0].style.justifyContent="space-between"
+hero3[0].style.padding="0 100px"
+hero3[0].style.background="#F2F2F2"
+
+
+const imgsss=document.getElementsByClassName("imgsss")
+console.log(imgsss)
+imgsss[0].style.display="flex"
+imgsss[0].style.margin="auto"
+
+imgsss[1].style.display="flex"
+imgsss[1].style.margin="auto"
+
+imgsss[2].style.display="flex"
+imgsss[2].style.margin="auto"
+
+
+imgsss[3].style.display="flex"
+imgsss[3].style.margin="auto"
+
+
+const teg3 =document.getElementsByTagName("h4")
+console.log(teg3)
+
+teg3[0].style.fontSize="18px"
+teg3[0].style.fontWeight="700"
+teg3[0].style.margin="35px 0 35px 0"
+
+teg3[1].style.fontSize="18px"
+teg3[1].style.fontWeight="700"
+teg3[1].style.margin="35px 0 35px 0"
+
+
+teg3[2].style.fontSize="18px"
+teg3[2].style.fontWeight="700"
+teg3[2].style.margin="35px 0 35px 0"
+
+
+teg3[3].style.fontSize="18px"
+teg3[3].style.fontWeight="700"
+teg3[3].style.margin="35px 0 35px 0"
+
+
+const teg4 =document.getElementsByTagName("p")
+console.log(teg4)
+teg4[7].style.width="312px"
+teg4[7].style.fontSize="14px"
+teg4[7].style.fontWeight="400"
+teg4[7].style.color="#434343"
+teg4[6].style.width="312px"
+teg4[6].style.fontSize="14px"
+teg4[6].style.fontWeight="400"
+teg4[6].style.color="#434343"
+
+teg4[5].style.width="312px"
+teg4[5].style.fontSize="14px"
+teg4[5].style.fontWeight="400"
+teg4[5].style.color="#434343"
+
+teg4[4].style.width="312px"
+teg4[4].style.fontSize="14px"
+teg4[4].style.fontWeight="400"
+teg4[4].style.color="#434343"
+
+
+// section2
+
+const section2=document.getElementsByClassName("section2")
+console.log(section2)
+section2[0].style.width="100%"
+section2[0].style.height="404px"
+section2[0].style.display="flex"
+section2[0].style.alignItems="center"
+section2[0].style.justifyContent="space-between"
+section2[0].style.padding="0 300px"
+section2[0].style.background="#000000"
+
+
+const teg5 =document.getElementsByTagName("h1")
+console.log(teg5)
+teg5[0].style.width="510px"
+teg5[0].style.color="#FFFFFF"
+teg5.style.width="662px"
+teg5.style.fontSize="42px"
